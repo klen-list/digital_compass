9fd7207e35097ca70704dab22a1993623aa5ba3a
klen-list <anton.burkov.02@mail.ru>
2021-11-25 02:31:15 +0300

    replaced spaces with tabs

--- lua/autorun/client/cl_arma3_compass.lua
+++ lua/autorun/client/cl_arma3_compass.lua
@@ -4,7 +4,7 @@ local mdl = Model"models/bohemia_arma3/compass.mdl"
 
 local grad = Material"gui/center_gradient"
 local compass_star_rotated = CreateMaterial("compass_star_rotated", "vertexlitgeneric", {
-    ["$basetexture"] = "models/bohemia_arma3/compass_base_star_ca"
+	["$basetexture"] = "models/bohemia_arma3/compass_base_star_ca"
 })
 
 surface.CreateFont("Compass3D", {
@@ -26,19 +26,19 @@ local cvar_up_offset = CreateClientConVar("compass_up_offs", 4)
 local cvar_pitch_offset = CreateClientConVar("compass_angle_offs", -40)
 
 concommand.Add("compass_reset_settings", function()
-    compass_digital_enable:SetInt(1)
-    compass_digital_color:SetString"255 255 255 150"
-    cvar_forward_offset:SetInt(11)
-    cvar_up_offset:SetInt(4)
-    cvar_pitch_offset:SetInt(-40)
-    Msg"Compass settings was reset to default.\n"
+	compass_digital_enable:SetInt(1)
+	compass_digital_color:SetString"255 255 255 150"
+	cvar_forward_offset:SetInt(11)
+	cvar_up_offset:SetInt(4)
+	cvar_pitch_offset:SetInt(-40)
+	Msg"Compass settings was reset to default.\n"
 end)
 
 local function GetDegMinSecFromGmodDeg(D)
-    D = math.abs(D - 360 * math.floor(D / 360))
-    local min = (D % 1) * 59
-    local sec = (min % 1) * 59
-    return math.floor(D), math.floor(min), math.floor(sec)
+	D = math.abs(D - 360 * math.floor(D / 360))
+	local min = (D % 1) * 59
+	local sec = (min % 1) * 59
+	return math.floor(D), math.floor(min), math.floor(sec)
 end
 
 local star_matrix = Matrix()
@@ -47,12 +47,12 @@ local rot_angle = Angle()
 local deg_delayed = Angle()
 
 local function SetCompassAngle(deg)
-    deg_delayed.y = deg
-    rot_angle = LerpAngle(.03, rot_angle, deg_delayed)
-    star_matrix:Translate(vec_origin)
-    star_matrix:SetAngles(rot_angle)
-    star_matrix:Translate(-vec_origin)
-    compass_star_rotated:SetMatrix("$basetexturetransform", star_matrix)
+	deg_delayed.y = deg
+	rot_angle = LerpAngle(.03, rot_angle, deg_delayed)
+	star_matrix:Translate(vec_origin)
+	star_matrix:SetAngles(rot_angle)
+	star_matrix:Translate(-vec_origin)
+	compass_star_rotated:SetMatrix("$basetexturetransform", star_matrix)
 end
 
 local DISABLED, pos, ang, angle = true, Vector(), Angle(), Angle()
@@ -60,86 +60,86 @@ local grad_col = Color(0, 0, 0, 255)
 local text_col = Color(255, 255, 255, 150)
 
 cvars.AddChangeCallback("compass_digital_color", function(_, __, new)
-    local r, g, b, a = string.match(new, "(%d+)% (%d+)% (%d+)% (%d+)")
-    if r and g and b and a then
-        text_col = Color(r, g, b, a)
-    end
+	local r, g, b, a = string.match(new, "(%d+)% (%d+)% (%d+)% (%d+)")
+	if r and g and b and a then
+		text_col = Color(r, g, b, a)
+	end
 end)
 
 local function RenderDigital()
-    ang:RotateAroundAxis(ang:Right(), -65)
-    ang:RotateAroundAxis(ang:Up(), 90)
+	ang:RotateAroundAxis(ang:Right(), -65)
+	ang:RotateAroundAxis(ang:Up(), 90)
 
-    cam.Start3D2D(pos - ang:Forward() * 2 - ang:Right() * 3.5, ang, .01)
-        surface.SetDrawColor(grad_col)
-        surface.SetMaterial(grad)
-        surface.DrawTexturedRect(0, 0, 400, 150)
+	cam.Start3D2D(pos - ang:Forward() * 2 - ang:Right() * 3.5, ang, .01)
+		surface.SetDrawColor(grad_col)
+		surface.SetMaterial(grad)
+		surface.DrawTexturedRect(0, 0, 400, 150)
 
-        render.PushFilterMag(TEXFILTER.ANISOTROPIC)
-        render.PushFilterMin(TEXFILTER.ANISOTROPIC)
-            local deg, min, sec = GetDegMinSecFromGmodDeg(angle)
+		render.PushFilterMag(TEXFILTER.ANISOTROPIC)
+		render.PushFilterMin(TEXFILTER.ANISOTROPIC)
+			local deg, min, sec = GetDegMinSecFromGmodDeg(angle)
 
-            surface.SetFont"Compass3D"
-            local x, y = surface.GetTextSize(deg)
+			surface.SetFont"Compass3D"
+			local x, y = surface.GetTextSize(deg)
 
-            surface.SetDrawColor(text_col.r, text_col.g, text_col.b, 128)
-            surface.DrawOutlinedRect(190 + x * .5, 20, 27, 27, math.floor(math.sin(CurTime() * 6) * 3) + 7)
+			surface.SetDrawColor(text_col.r, text_col.g, text_col.b, 128)
+			surface.DrawOutlinedRect(190 + x * .5, 20, 27, 27, math.floor(math.sin(CurTime() * 6) * 3) + 7)
 
-            draw.DrawText(deg, "Compass3D", 174, 0, text_col, TEXT_ALIGN_CENTER)
-            draw.DrawText((deg == 0 or deg == 360) and "N" or
-                deg < 90 and "NE" or
-                deg == 90 and "E" or 
-                deg < 180 and "SE" or -- looks bad, i know
-                deg == 180 and "S" or 
-                deg < 270 and "SW" or
-                deg == 270 and "W" or
-            deg < 360 and "NW", "Compass3DSmall", 190 + x * .5, 20 + 27 + 7, text_col) 
-            draw.DrawText(string.format("%d'%d''", min, sec), "Compass3DSmall", 190 + x * .5, 20 + 27 + 7 + 37, text_col)
-        render.PopFilterMag()
-        render.PopFilterMin()
-    cam.End3D2D()
+			draw.DrawText(deg, "Compass3D", 174, 0, text_col, TEXT_ALIGN_CENTER)
+			draw.DrawText((deg == 0 or deg == 360) and "N" or
+				deg < 90 and "NE" or
+				deg == 90 and "E" or 
+				deg < 180 and "SE" or -- looks bad, i know
+				deg == 180 and "S" or 
+				deg < 270 and "SW" or
+				deg == 270 and "W" or
+			deg < 360 and "NW", "Compass3DSmall", 190 + x * .5, 20 + 27 + 7, text_col) 
+			draw.DrawText(string.format("%d'%d''", min, sec), "Compass3DSmall", 190 + x * .5, 20 + 27 + 7 + 37, text_col)
+		render.PopFilterMag()
+		render.PopFilterMin()
+	cam.End3D2D()
 end
 
 local function Arma3CompassRender()
-    if DISABLED then return end
+	if DISABLED then return end
 
-    if not CompassCEnt then
-        CompassCEnt = ClientsideModel(mdl)
-        CompassCEnt:Spawn()
-    end
+	if not CompassCEnt then
+		CompassCEnt = ClientsideModel(mdl)
+		CompassCEnt:Spawn()
+	end
 
-    local ply = LocalPlayer()
-    ang = ply:EyeAngles()
-    pos = ply:EyePos()
-    angle = ang.y
+	local ply = LocalPlayer()
+	ang = ply:EyeAngles()
+	pos = ply:EyePos()
+	angle = ang.y
 
-    SetCompassAngle(angle)
+	SetCompassAngle(angle)
 
-    pos = pos + ang:Forward() * cvar_forward_offset:GetFloat() - ang:Up() * cvar_up_offset:GetFloat()
+	pos = pos + ang:Forward() * cvar_forward_offset:GetFloat() - ang:Up() * cvar_up_offset:GetFloat()
 
-    ang:RotateAroundAxis(ang:Up(), 180)
-    ang:RotateAroundAxis(ang:Right(), cvar_pitch_offset:GetFloat())
+	ang:RotateAroundAxis(ang:Up(), 180)
+	ang:RotateAroundAxis(ang:Right(), cvar_pitch_offset:GetFloat())
 
-    CompassCEnt:SetRenderOrigin(pos)
-    CompassCEnt:SetRenderAngles(ang)
+	CompassCEnt:SetRenderOrigin(pos)
+	CompassCEnt:SetRenderAngles(ang)
 
-    CompassCEnt:SetNoDraw(true) -- for safe, because other shitty addons can enable draw at any moment
-    
-    cam.Start3D()
-        render.MaterialOverrideByIndex(2, compass_star_rotated)
-        CompassCEnt:DrawModel()
+	CompassCEnt:SetNoDraw(true) -- for safe, because other shitty addons can enable draw at any moment
+	
+	cam.Start3D()
+		render.MaterialOverrideByIndex(2, compass_star_rotated)
+		CompassCEnt:DrawModel()
 
-        if cvar_digital_enabled:GetBool() then
-            ProtectedCall(RenderDigital) -- does not allow skipping render.PopFilter* and cam.End3D2D due to an stack call break
-        end
-    cam.End3D()
+		if cvar_digital_enabled:GetBool() then
+			ProtectedCall(RenderDigital) -- does not allow skipping render.PopFilter* and cam.End3D2D due to an stack call break
+		end
+	cam.End3D()
 end
 hook.Add("HUDPaint", "Arma3CompassRender", Arma3CompassRender)
 
 concommand.Add("+show_compass", function()
-    DISABLED = false
+	DISABLED = false
 end)
 
 concommand.Add("-show_compass", function()
-    DISABLED = true
+	DISABLED = true
 end)
